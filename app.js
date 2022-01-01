f9681b6cbe14aa3a3654d33e2192b02bfb04e9ff
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-02 00:37:11 +0800

    增加注释

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Logger = require("./log");
-const logger = new Logger();
+const Logger = require("./log"); //导入从log.js文件导出的class
+const logger = new Logger(); //创建class类对象实例
 logger.on("事件", (thingData) => {
   console.log("捕获到一个事件,这些是此事件发出的参数:", thingData);
-});
-logger.log("信息");
+}); //设置捕获到事件的响应处理方式;
+logger.log("信息"); //抛出一个响应来验证是否正常运行
--- log.js
+++ log.js
@@ -1,10 +1,11 @@
-const EventEmitter = require("events");
+const EventEmitter = require("events"); //导入事件处理的node自带模块
 
-const url = "http://www.ventroar.xyz";
+const url = "http://www.ventroar.xyz"; //随便设置的一些参数
 class Logger extends EventEmitter {
+  //将Logger类继承自EventEmitter事件处理类
   log(msg) {
     console.log(`this msg is ${msg}`);
-    this.emit("事件", { id: 1, url: url });
+    this.emit("事件", { id: 1, url: url }); //发起一个事件及传出一些参数,这里传出了一个Obj;
   }
 }
 
