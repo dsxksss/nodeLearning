b7be9d8468d7b5d1ac3d2a2a7db6ca3927fafa7d
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-02 11:48:58 +0800

    express框架的使用:设置get的基本用法

---
+++ express-demo/index.js
@@ -0,0 +1,29 @@
+const express = require("express"); //导入express轻量级框架
+
+const setApi = express(); //创建fucntion实例
+
+const userData = [
+  { id: 1, name: "Jack1", age: 18 },
+  { id: 2, name: "Jack2", age: 20 },
+  { id: 3, name: "Jack3", age: 22 },
+];
+
+//获取整个数据
+//req:获取的对方数据,res:服务端的响应反馈
+setApi.get("/api/userData", (req, res) => {
+  res.send(`<h1>你好</h1>`);
+});
+
+//获取单个数据
+//parseInt()函数是将一个字符串转换成int类型整数
+setApi.get("/api/userData/:id", (req, res) => {
+  //查找对应内容并返回找到的数据
+  const ByOneData = userData.find((c) => c.id === parseInt(req.params.id));
+  //如果不存在对应内容的话，向客户端发送一个404错误状态码，并提示不存在此数据
+  if (!ByOneData) res.status(404).send(`<h1>erreo! NotFund This Data!!!</h1>`);
+  res.send(ByOneData);
+});
+
+//创建端口监听
+const host = process.env.PORT || 3000; //设置动态端口
+setApi.listen(host, () => console.log(`listen to ${host} host ...`));
