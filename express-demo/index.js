1b9989c6de7f6591a45d1fb2779663392c5b834d
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-02 14:28:58 +0800

    put数据的基本操作+joi验证库封装

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,16 @@
 const express = require("express"); //导入express轻量级框架
+const Joi = require("joi");
 
 const setApi = express(); //创建fucntion实例
 setApi.use(express.json()); //中间件
+
+//FUNCTION:封装的数据验证函数
+const dataByTrue = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(data);
+};
 
 let userData = [
   { id: 1, name: "Jack1", age: 18 },
@@ -28,6 +37,14 @@ setApi.get("/api/userData/:id", (req, res) => {
 
 //post部分:
 setApi.post("/api/userData", (req, res) => {
+  //数据验证
+  const { error } = dataByTrue(req.body);
+  //如果数据验证出现错误就返回400状态码,并说明具体问题
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
   const oneData = {
     id: userData.length + 1,
     name: req.body.name,
@@ -35,6 +52,25 @@ setApi.post("/api/userData", (req, res) => {
   };
   userData.push(oneData);
   res.send(oneData);
+});
+
+//put部分:
+setApi.put("/api/userData/:id", (req, res) => {
+  //查找用户要更新的具体数据
+  const ByOneData = userData.find((c) => c.id === parseInt(req.params.id));
+  //如果不存在对应内容的话，向客户端发送一个404错误状态码，并提示不存在此数据
+  if (!ByOneData) res.status(404).send(`<h1>erreo! NotFund This Data!!!</h1>`);
+
+  //数据验证
+  const { error } = dataByTrue(req.body);
+  //如果数据验证出现错误就返回400状态码,并说明具体问题
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  ByOneData.name = req.body.name;
+  res.send(ByOneData);
 });
 
 //创建端口监听
