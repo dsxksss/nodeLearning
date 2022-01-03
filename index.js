58ab06914ebba588a646665e226cb329f3fbe209
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-03 15:35:28 +0800

    应用配置管理库:config基本使用，创建了三个环境配置文件，（默认、开发版、发行版），增加注释

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"); //导入express框架
 const Joi = require("joi"); //导入数据验证库
+const config = require("config"); //导入应用配置管理库
 // const logger = require("./logger"); //导入自定义中间件
 const helmet = require("helmet"); //helmet是加强http头安全性的一个中间件
 const morgan = require("morgan"); //morgan是一个记录http请求日志的中间件
@@ -8,6 +9,10 @@ const api = express(); //创建框架实例
 // process.env.NODE_ENV;//node内置显示当前环境配置的,如果没有设置,默认为undefined
 // express.get("env");//exoress内置的get函数也可以获取当前系统的环境配置
 
+//Configuration
+console.log(`MY APP name: ${config.get("name")}`);
+console.log(`mail host: ${config.get("mail.host")}`);
+
 //创建中间件:中间件是在一个客户端连入之后必须经过的一个过程;
 //此中间件的作用是将一个刚连上服务的客户端里的数据转换成req.body的JSON数据供我们使用.
 api.use(express.json());
@@ -15,7 +20,7 @@ api.use(helmet());
 //检测当前环境是否为开发环境，如果是，则使用morgan日志记录
 if (api.get("env") === "development") {
   api.use(morgan("dev")); //tiny是简单的log记录方式,这里使用的是dev记录格式
-  console.log("development!,morgan starting~");
+  console.log("development !,morgan starting~");
 }
 
 //logger中间件是自己创建的自定义中间件
