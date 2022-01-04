8b8b73569e676a535cc7c7fff57273498caffd24
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-01-04 16:38:22 +0800

    整理文件，增加/修改部分注释

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const debug = require("debug")("app_debug");
 const express = require("express"); //导入express框架
 const courses = require("./routers/courses");
 const config = require("config"); //导入应用配置管理库
-// const logger = require("./logger"); //导入自定义中间件
+// const logger = require("./Middleware/logger"); //导入自定义中间件
 const helmet = require("helmet"); //helmet是加强http头安全性的一个中间件
 const morgan = require("morgan"); //morgan是一个记录http请求日志的中间件
 const api = express(); //创建框架实例
@@ -19,7 +19,7 @@ debug(`mail host: ${config.get("mail.host")}`);
 //此中间件的作用是将一个刚连上服务的客户端里的数据转换成req.body的JSON数据供我们使用.
 api.use(express.json());
 api.use(helmet());
-api.use("/api/ventroar/", courses); //表示只使用此路由功能，第一个参数为默认路径
+api.use("/api/ventroar/", courses); //表示使用从其他文件导出的路由功能，第一个参数为默认路由path
 
 //检测当前环境是否为开发环境，如果是，则使用morgan日志记录
 if (api.get("env") === "development") {
@@ -28,7 +28,7 @@ if (api.get("env") === "development") {
 }
 
 //logger中间件是自己创建的自定义中间件
-// api.use(logger);
+// api.use(./Middleware/logger);
 
 //static中间件是向网页提供静态数据
 /*静态数据是存在与网站根目录到
